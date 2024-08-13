09584f26235917e0dc469f87c263135d7ecfee19
Yunus <ibadovunus55@gmail.com>
2024-08-14 02:44:49 +0500

    fixed

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -7,8 +7,45 @@ function Layout({ children }) {
         aside.classList.toggle("hide_aside")
     }
 
+    const [First_bg, setFirst_bg] = useState("white")
+    const [First_a, setFirst_a] = useState("black")
+    const [Second_bg, setSecond_bg] = useState(" rgb(63, 63, 63)")
+    const [Second_a, setSecond_a] = useState("white")
+    const [Therd_bg, setTherd_bg] = useState(" rgb(63, 63, 63)")
+    const [Therd_a, setTherd_a] = useState("white")
 
 
+    const first_change = () => {
+        setFirst_bg("white" )
+        setFirst_a("black")
+
+        setSecond_bg("rgb(63, 63, 63)")
+        setTherd_bg("rgb(63, 63, 63)")
+        setSecond_a("white")
+        setTherd_a("white")
+    }
+
+    
+    const second_change = () => {
+        setSecond_bg("white")
+        setSecond_a("black")
+
+        setFirst_bg("rgb(63, 63, 63)")
+        setTherd_bg("rgb(63, 63, 63)")
+        setFirst_a("white")
+        setTherd_a("white")
+    }
+
+    
+    const therd_change = () => {
+        setTherd_bg("white")
+        setTherd_a("black")
+
+        setSecond_bg("rgb(63, 63, 63)")
+        setFirst_bg("rgb(63, 63, 63)")
+        setSecond_a("white")
+        setFirst_a("white")
+    }
 
     
 
@@ -89,20 +126,20 @@ function Layout({ children }) {
                             </div>
 
                             <div className="sec">
-                                <ul>
+                                <ul className="sec_ul">
 
-                                    <li id="1" className="cat">
-                                        <a className="cat_a" id="11" href="#">Всё</a>
+                                    <li id="um" onClick={first_change} style={{backgroundColor : First_bg}} className="cat">
+                                        <a style={{color:First_a}} className="cat_a" id="11" href="#">Всё</a>
                                     </li>
 
 
-                                    <li id="2" className="cat">
-                                        <a  className="cat_a" id="22" href="#">Музыка</a>
+                                    <li onClick={second_change} style={{backgroundColor:Second_bg}} className="cat">
+                                        <a  style={{color:Second_a}} className="cat_a" id="22" href="#">Музыка</a>
                                     </li>
 
 
-                                    <li id="3" className="cat">
-                                        <a className="cat_a" id="33" href="#">Подкасты</a>
+                                    <li onClick={therd_change} style={{backgroundColor: Therd_bg}} className="cat">
+                                        <a style={{color:Therd_a}} className="cat_a" id="33" href="#">Подкасты</a>
                                     </li>
                                 </ul>
 
